447653a44a7db098b4d9bbb65da9cb9027320912
l397286219@yeah.net <l397286219@yeah.net>
2019-12-05 21:39:55 +0800

    调整逻辑

--- main.go
+++ main.go
@@ -18,40 +18,45 @@ var (
 	port = flag.String("p", "23456", "iris http port")
 )
 
-// 加载crontab
-func crontab() {
-	for _, cmd := range config.Conf.Cmds {
-		cronStr := cmd.CronStr
-		title := cmd.Title
-		execCmd := cmd.ExeCmd
-		receivers := cmd.Receivers
-		attach := cmd.AttachFile
-		timer.AddCrontab(cronStr, title, func(args ...interface{}) {
-			go func() {
-				// 异步执行
-				start := time.Now()
-				log.Printf("%+v\n", cmd)
-				if cmd.Valid {
-					if res, err := basic.Exec(execCmd); err != nil {
-						mail.SendMsg(title, fmt.Sprintf("指令执行失败: %+v。\n", err), execCmd, attach, receivers)
-					} else {
-						execCmd = fmt.Sprintf("执行时长: %d 执行指令: %s", time.Now().Sub(start).Seconds(), execCmd)
-						mail.SendMsg(title, string(res), execCmd, attach, receivers)
-					}
-				} else {
-					mail.SendMsg(title, "该项配置未开启。\n", execCmd, "", receivers)
-				}
-			}()
-		})
+// 计划任务执行
+func task(cmd config.Cmd) {
+	title := cmd.Title
+	execCmd := cmd.ExeCmd
+	receivers := cmd.Receivers
+	attach := cmd.AttachFile
+	start := time.Now()
+	log.Printf("%+v\n", cmd)
+	if cmd.Valid {
+		if res, err := basic.Exec(execCmd); err != nil {
+			mail.SendMsg(title, fmt.Sprintf("指令执行失败: %+v %+v。\n", res, err), execCmd, attach, receivers)
+		} else {
+			execCmd = fmt.Sprintf("执行时长: %vs 执行指令: %s", time.Now().Sub(start).Seconds(), execCmd)
+			mail.SendMsg(title, string(res), execCmd, attach, receivers)
+		}
+	} else {
+		mail.SendMsg(title, "该项配置未开启。\n", execCmd, "", receivers)
 	}
+}
+
+// 计划任务配置
+func crontab(cmd config.Cmd) {
+	cronStr := cmd.CronStr
+	title := cmd.Title
+	timer.AddCrontab(cronStr, title, func(args ...interface{}) {
+		// 异步执行
+		go task(cmd)
+	})
 }
 
 // 加载另外的httpHandle
 func InitApi(app *iris.Application) {
 }
 
+// 加载crontab
 func init() {
-	crontab()
+	for _, cmd := range config.Conf.Cmds {
+		crontab(cmd)
+	}
 }
 
 func main() {
